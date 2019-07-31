1bcb721626252174908e71868438e4408202abdc
William Casey <casey2@stolaf.edu>
2019-07-30 22:04:10 -0500

    responsive navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,16 +2,39 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import '../styles/Navbar.css';
 
-const Navbar = () => {
-    return (
-        <div className='navbar'>
-            <NavLink exact to='/'>Home</NavLink>
-            <NavLink exact to='/projects'>Projects</NavLink>
-            <NavLink exact to='/blog'>Blog</NavLink>
-            <NavLink exact to='/about'>About</NavLink>
-            <NavLink exact to='/contact'>Contact</NavLink>
-        </div>
-    );
+class Navbar extends React.Component {
+    constructor() {
+        super();
+        this.state = {extended: true};
+    }
+
+    toggleExtended = () => {
+        this.setState({extended: !this.state.extended});
+    }
+
+    render() {
+        return (
+            <div className='nav-wrapper'> { this.state.extended 
+              ? <div className='navbar'>
+                    <div className='desktop-only'>
+                        ICON
+                    </div>
+                    <div>
+                        <NavLink exact to='/'>Home</NavLink>
+                        <NavLink exact to='/projects'>Projects</NavLink>
+                        <NavLink exact to='/blog'>Blog</NavLink>
+                        <NavLink exact to='/about'>About</NavLink>
+                        <NavLink exact to='/contact'>Contact</NavLink>
+                    </div>
+                    <div className='desktop-only'>
+                        ICON
+                    </div>
+                </div> 
+              : null }
+                <div className='menu-toggle' onClick={this.toggleExtended}>MENU</div>
+            </div>
+        );
+    }
 }
 
 export default Navbar;
