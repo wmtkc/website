d84c05c93794daeed5aab5cb1ea3ab69c190af8e
William Casey <wcasey@fishbowlsolutions.com>
2019-08-12 15:48:07 -0500

    case sensitivity build

---
+++ src/pages/blog.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import dateFormat from 'dateformat';
+import PostCard from '../components/PostCard';
+import '../styles/BlogList.css';
+import noCover from '../../public/uploads/no-cover.jpg';
+import { useRouteData } from 'react-static';
+
+const BlogList = () => {
+    const { posts } = useRouteData();
+    const cards = [];
+    posts.forEach((post, index) => {
+        if (post.data.published) {
+            cards.push(<PostCard key={index}
+                                 url={post.data.url}
+                                 title={post.data.title} 
+                                 coverImg={post.data.thumbnail ? post.data.thumbnail : noCover} 
+                                 date={dateFormat(post.data.date, 'mmmm dS, yyyy')} />)
+        }
+    });
+    return (
+        <div className='posts'>
+            {cards}
+        </div>
+    );
+}
+
+export default BlogList;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/pages/projects.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/Content.css';
+
+const Projects = () => {
+    return (
+        <div className='content'>
+            <span className='header'>Projects</span>
+        </div>
+    );
+}
+
+export default Projects;
